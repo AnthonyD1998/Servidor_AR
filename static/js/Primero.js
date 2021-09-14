73a15855a1c042430dcd89b18ea2ca4da12b0728
AnthonyD1998 <76930321+AnthonyD1998@users.noreply.github.com>
2021-09-14 11:21:42 -0500

    Add files via upload

---
+++ static/js/Primero.js
@@ -0,0 +1,9 @@
+alert('Eso no es:')
+function LED1_On(){
+	console.log('Se presiono el boton_1')
+	document.getElementById('sensor').innerHTML='LED ON'
+;}
+function LED1_Off(){
+	console.log('Se presiono el boton_2')
+	document.getElementById('sensor').innerHTML='LED OFF'
+;}
